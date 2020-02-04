0828afd3f7e3919a835fbb2ec183c2794a276253
Kim HeeJae <iowa329@gmail.com>
2020-02-04 14:50:29 +0900

    [UPDATE] remove HeaderView in main NewsFeed

--- Nutee/Source/ViewControllers/NewsFeed/NewsFeedVC.swift
+++ Nutee/Source/ViewControllers/NewsFeed/NewsFeedVC.swift
@@ -29,10 +29,6 @@ class NewsFeedVC: UIViewController {
         newsTV.dataSource = self
 
         initColor()
-        
-        // Register the custom header view which Nib 'PostingTableHeaderSection'
-        let nib = UINib(nibName: "PostingTableHeaderSection", bundle: nil)
-        self.newsTV.register(nib, forHeaderFooterViewReuseIdentifier: "PostingTableHeaderSection")
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -57,8 +53,6 @@ class NewsFeedVC: UIViewController {
     
 }
 
-extension NewsFeedVC : UITextViewDelegate { }
-
 // MARK: - UITableView
 extension NewsFeedVC : UITableViewDelegate { }
 
@@ -69,40 +63,17 @@ extension NewsFeedVC : UITableViewDataSource {
         self.tabBarController?.tabBar.tintColor = UIColor.white
     }
     
-    /*func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return "여기는 해더 뷰 제목"
-    }*/
-    
-    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-    
-        // Dequeue with the reuse identifier
-        let cell = tableView.dequeueReusableHeaderFooterView(withIdentifier: "PostingTableHeaderSection")
-
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return 160
-    }
-    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 10
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        let row : [NewsFeedVO] = newsList
-        
         //Custom셀인 'NewsFeedCell' 형식으로 변환
         let cell = tableView.dequeueReusableCell(withIdentifier: "newsFeedCell", for: indexPath) as! NewsFeedCell
         
         if indexPath.row % 2 == 1 {
             cell.backgroundColor = .lightGray
         }
-        //데이터 소스에 저장된 값을 커스텀 한 NewsFeedCell의 아울렛 변수들에게 전달
-//        cell.lblUserId.text = row[indexPath.row].userId
-//        cell.lblContents.text = row[indexPath.row].contents
-//        cell.imgUserImg.image = UIImage(named: row[indexPath.row].userImg!)
-//        cell.lblPostTime.text = row[indexPath.row].postTime
         
         return cell
     }
@@ -112,10 +83,7 @@ extension NewsFeedVC : UITableViewDataSource {
         
         let detailNewsFeedSB = UIStoryboard(name: "DetailNewsFeed", bundle: nil)
         let showDetailNewsFeedVC = detailNewsFeedSB.instantiateViewController(withIdentifier: "DetailNewsFeed") as! DetailNewsFeedVC
-//        self.present(showDetailNewsFeedVC, animated: true, completion: nil)
         self.navigationController?.pushViewController(showDetailNewsFeedVC, animated: true)
     }
     
 }
-
-// MARK: -
