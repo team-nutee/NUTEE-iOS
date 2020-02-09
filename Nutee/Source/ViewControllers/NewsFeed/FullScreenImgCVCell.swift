c36278d8d846b507f37c5caaff6feb38beb60708
Kim HeeJae <iowa329@gmail.com>
2020-02-09 18:26:23 +0900

    [CREATE] CollectionView를 위한 테스트 파일

---
+++ Nutee/Source/ViewControllers/NewsFeed/FullScreenImgCVCell.swift
@@ -0,0 +1,16 @@
+
+//
+//  FullScreenImgCell.swift
+//  Nutee
+//
+//  Created by Hee Jae Kim on 2020/02/09.
+//  Copyright © 2020 S.OWL. All rights reserved.
+//
+
+import UIKit
+
+class FullScreenImgCVCell: UICollectionViewCell {
+    
+    @IBOutlet var imgvwFullScreen: UIImageView!
+    
+}
