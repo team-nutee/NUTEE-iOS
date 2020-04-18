e672cc1ce615f328ef161d81afca8c39b1c37a48
오준현 <oh.junhyeon95@gmail.com>
2020-04-18 14:55:06 +0900

    update : 이미지 처리 익스텐션 추가

--- Nutee/Source/Extensions/UIImageView+Extension.swift
+++ Nutee/Source/Extensions/UIImageView+Extension.swift
@@ -13,6 +13,7 @@ import UIKit
 // Kingfisher를 이용하여 url로부터 이미지를 가져오는 extension
 extension UIImageView {
 
+    // http://15.164.50.161:9425/settings/nutee_profile.png
     public func imageFromUrl(_ urlString: String?, defaultImgPath : String?) {
 
         let tmpUrl : String?
@@ -31,6 +32,28 @@ extension UIImageView {
         }
 
     }
+    
+    func setImageNutee(_ urlString: String?){
+        if urlString == ""{
+            setImage(with: "http://15.164.50.161:9425/settings/nutee_profile.png")
+        } else {
+            setImage(with: APIConstants.BaseURL + "/"  + (urlString ?? ""))
+        }
+    }
+
+    func setImage(with urlString: String) {
+        let cache = ImageCache.default
+        cache.retrieveImage(forKey: urlString, options: nil) { (image, _) in // 캐시에서 키를 통해 이미지를 가져온다.
+            if let image = image { // 만약 캐시에 이미지가 존재한다면
+                self.image = image // 바로 이미지를 셋한다.
+            } else {
+                let url = URL(string: urlString) // 캐시가 없다면
+                let resource = ImageResource(downloadURL: url! , cacheKey: urlString) // URL로부터 이미지를 다운받고 String 타입의 URL을 캐시키로 지정하고
+                self.kf.setImage(with: resource) // 이미지를 셋한다.
+            }
+        }
+    }
+
 
     
 }
